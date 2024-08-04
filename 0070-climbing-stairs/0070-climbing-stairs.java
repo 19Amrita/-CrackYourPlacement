cd864c6ee65bc667103c34024d7e8720a8ceb76d
19Amrita <121977206+19Amrita@users.noreply.github.com>
2024-08-04 23:13:21 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (31.31%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+       // if(n==0)return 0;
+     int dp[]=new int[n];
+        if(n==1)return 1;
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
+    }
+}
